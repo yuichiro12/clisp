a48b3c05bf1b49b2adc6e8f0cc4f5ae0f5f27e57
yuichiro12 <talcnrhyme@gmail.com>
2016-02-23 14:23:48 +0900

    error-handling

---
+++ error.lisp
@@ -0,0 +1,14 @@
+(define-condition foo () ()
+  (:report (lambda (condition stream)
+	     (princ "Stop FOOing around, numbskull!" stream))))
+
+(defun bad-function ()
+  (error 'foo))
+
+(handler-case (bad-function)
+  (foo () "somebody signaled foo!")
+  (bar () "somebody signaled bar!"))
+
+(unwind-protect (/ 1 0)
+  (princ "I need to say 'flubyduby' matter what"))
+
