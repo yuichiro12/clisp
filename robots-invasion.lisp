cab1ecbf0c315bc42cfeb3676388ffdc8424058d
yuichiro12 <talcnrhyme@gmail.com>
2016-02-23 14:23:48 +0900

    chapter 11

--- robots-invasion.lisp
+++ robots-invasion.lisp
@@ -7,4 +7,37 @@
 		 (force-output)
 		 (let* ((c (read))
 			(d (assoc c directions)))
-		   (cond (d (+ pos (cdr d))))))))
+		   (cond (d (+ pos (cdr d)))
+			 ((eq 't c) (random 1024))
+			 ((eq 'l c) (return-from main 'bye))
+			 (t pos))))
+     for monsters = (loop repeat 10
+		       collect (random 1024))
+     then (loop for mpos in monsters
+	     collect (if (> (count mpos monsters) 1)
+			 mpos
+			 (cdar (sort (loop for (k . d) in directions
+					for new-mpos = (+ mpos d)
+					collect (cons (+ (abs (- (mod new-mpos 64)
+								 (mod pos 64)))
+							 (abs (- (ash new-mpos -6)
+								 (ash pos -6))))
+						      new-mpos))
+				     '<
+				     :key #'car))))
+     when (loop for mpos in monsters
+	     always (> (count mpos monsters) 1))
+     return 'player-wins
+     do (format t
+		"~%|~{~<|~%|~,65:;~A~>~}|"
+		(loop for p
+		   below 1024
+		   collect (cond ((member p monsters)
+				  (cond ((= p pos) (return-from main 'player-loses))
+					((> (count p monsters) 1) #\#)
+					(t #\A)))
+				 ((= p pos)
+				  #\@)
+				 (t
+				  #\ ))))))
+
