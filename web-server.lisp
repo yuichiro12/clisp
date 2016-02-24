0c5ea45686e66b837db5637204735f68a5ad5146
yuichiro12 <talcnrhyme@gmail.com>
2016-02-24 12:34:30 +0900

    multibite-char decoding and request parameter list decoding

--- web-server.lisp
+++ web-server.lisp
@@ -1,18 +1,28 @@
-(defun http-char (c1 c2 &optional (default #\Space))
+(defun http-byte (c1 c2 &optional (default #.(char-code #\space)))
   (let ((code (parse-integer
-	       (coerce (list c1 c2) 'string)
+	       (coerce (list (code-char c1) (code-char c2)) 'string)
 	       :radix 16
 	       :junk-allowed t)))
-    (if code
-	(code-char code)
-	default)))
+    (or code default)))
 
 (defun decode-param (s)
   (labels ((f (lst)
 	     (when lst
 	       (case (car lst)
-		 (#\% (cons (http-char (cadr lst) (caddr lst))
+		 (#.(char-code #\%) (cons (http-byte (cadr lst) (caddr lst))
 			    (f (cdddr lst))))
-		 (#\+ (cons #\space (f (cdr lst))))
+		 (#.(char-code #\+) (cons #.(char-code #\space) (f (cdr lst))))
 		 (otherwise (cons (car lst) (f (cdr lst))))))))
-    (coerce (f (coerce s 'list)) 'string)))
+    (sb-ext:octets-to-string
+     (coerce (f (coerce (sb-ext:string-to-octets s :external-format :utf-8) 'list))
+	     '(vector(unsigned-byte 8)))
+     :external-format :utf-8)))
+
+(defun parse-params (s)
+  (let ((i1 (position #\= s))
+	(i2 (position #\& s)))
+    (cond (i1 (cons (cons (intern (string-upcase (subseq s 0 i1)))
+			  (decode-param (subseq s (1+ i1) i2)))
+		    (and i2 (parse-params (subseq s (1+ i2))))))
+	  ((equal s "") nil)
+	  (t s))))
